e7f92fa264136cc86076849dd17c77ab8e4f070f
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-08-03 11:27:12 +0700

    Update calculate_time.py

--- calculate_time.py
+++ calculate_time.py
@@ -4,6 +4,7 @@ import os
 
 GITHUB_USERNAME = 'volumeee'
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+README_FILE = 'README.md'
 
 def get_repos(username):
     url = f'https://api.github.com/users/{username}/repos'
@@ -30,12 +31,28 @@ def calculate_time_spent():
     
     return language_times
 
+def update_readme(language_times):
+    with open(README_FILE, 'r') as f:
+        readme_content = f.read()
+    
+    start_marker = '<!-- language_times_start -->'
+    end_marker = '<!-- language_times_end -->'
+
+    new_content = '## Time Spent on Projects by Dominant Language\n'
+    for language, time in language_times.items():
+        new_content += f'- **{language}**: {time} hours\n'
+
+    if start_marker in readme_content and end_marker in readme_content:
+        new_readme_content = readme_content.split(start_marker)[0] + start_marker + '\n' + new_content + '\n' + end_marker + readme_content.split(end_marker)[1]
+    else:
+        new_readme_content = readme_content + '\n' + start_marker + '\n' + new_content + '\n' + end_marker
+
+    with open(README_FILE, 'w') as f:
+        f.write(new_readme_content)
+
 def main():
     language_times = calculate_time_spent()
-    with open('language_times.md', 'w') as f:
-        f.write('## Time Spent on Projects by Dominant Language\n')
-        for language, time in language_times.items():
-            f.write(f'- **{language}**: {time} hours\n')
+    update_readme(language_times)
 
 if __name__ == '__main__':
     main()
